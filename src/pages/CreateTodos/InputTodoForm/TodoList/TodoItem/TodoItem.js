1152db3d01a7311f2251801e81a6ee7046090829
Yang Jae Hoon <didwogns789@naver.com>
2020-10-07 11:26:45 +0900

    Activate checkbox of todo

--- src/pages/CreateTodos/InputTodoForm/TodoList/TodoItem/TodoItem.js
+++ src/pages/CreateTodos/InputTodoForm/TodoList/TodoItem/TodoItem.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../../../../components/Button/Button";
 
 import classes from "./TodoItem.module.scss";
 
 const TodoItem = (props) => {
+  const [todoClass, setTodoClass] = useState(null);
+
   const handleClicked = () => {
     props.deleted(props.index);
   };
 
+  const handleChecked = (event) => {
+    console.log(event.target.checked);
+    if (event.target.checked) {
+      setTodoClass(classes.Todo);
+    } else {
+      setTodoClass(null);
+    }
+  };
+
   return (
     <div className={classes.TodoItem}>
-      <span>{props.todo}</span>
+      <span className={todoClass}>{props.todo}</span>
       <div className={classes.Controls}>
-        <input type="checkbox" />
+        <input type="checkbox" onClick={handleChecked} />
         <Button clicked={handleClicked}>
           <span style={{ fontSize: "20px" }} role="img" aria-label="delete">
             ❌
