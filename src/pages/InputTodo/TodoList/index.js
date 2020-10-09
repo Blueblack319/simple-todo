ecf08fc895a8558ef05771bfd80b8ff5380120b6
Yang Jae Hoon <didwogns789@naver.com>
2020-10-09 10:20:20 +0900

    Restrict TodoList height for mobile

--- src/pages/InputTodo/TodoList/index.js
+++ src/pages/InputTodo/TodoList/index.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import TodoItem from "./TodoItem";
 
 import classes from "./TodoList.module.scss";
 
 const TodoList = (props) => {
+  const [todoListClasses, setTodoListClasses] = useState([classes.TodoList]);
+
+  const listRef = useRef(null);
+
   const todos = props.todos.map((todoItem) => {
     return (
       <TodoItem
@@ -14,7 +18,30 @@ const TodoList = (props) => {
       />
     );
   });
-  return <div className={classes.TodoList}>{todos}</div>;
+
+  const setHeight = useCallback(() => {
+    const { current } = listRef;
+    if(todoListClasses.includes(classes.HeightLimit)){
+      return
+    }
+    if (current.clientHeight > 80) {
+      setTodoListClasses(todoListClasses.concat(classes.HeightLimit));
+      console.log("Here");
+    }
+    console.log(current.clientHeight)
+  }, [setTodoListClasses, todoListClasses]);
+
+  useEffect(() => {
+    setHeight()
+  })
+
+  console.log(todoListClasses);
+
+  return (
+    <div className={todoListClasses.join(" ")} ref={listRef}>
+      {todos}
+    </div>
+  );
 };
 
 export default TodoList;
